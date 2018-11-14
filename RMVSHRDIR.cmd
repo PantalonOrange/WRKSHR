81ce338f60e00c0a12fe2f030ea6741660ead1e6
Christian Brunner <31685593+prsbrc@users.noreply.github.com>
2018-11-14 08:03:55 +0100

    move to src-pf

--- RMVSHRDIR.cmd
+++
@@ -1,5 +0,0 @@
-             CMD        PROMPT('Netzfreigabe entfernen') +
-                          TEXT('Netzfreigabe entfernen') +
-                          ALWLMTUSR(*NO) AUT(*EXCLUDE)
-             PARM       KWD(NAME) TYPE(*NAME) LEN(12) MIN(1) +
-                          PROMPT('Freigabename')
